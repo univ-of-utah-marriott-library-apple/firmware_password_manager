5cca060345127ea799a07d839243aab1464fd817
Todd McDaniel <lazymutt@mac.com>
2016-03-07 12:34:31 -0700

    doc fixes.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='Firmware Password Manager',
-    version='2.0.0',
+    version='2.1.0',
     url='https://github.com/univ-of-utah-marriott-library-apple/firmware_password_manager',
     author='Todd McDaniel, Marriott Library IT Services',
     author_email='mlib-its-mac-github@lists.utah.edu',
     description=('A Python script to help Macintosh administrators manage the firmware ',
                  'passwords of their computers.'),
     license='MIT',
-    scripts=['firmware_password_manager.py'],
+    scripts=['firmware_password_manager.py', 'obfuscate_keylist.py'],
     classifiers=[
         'Development Status :: 5 - Stable',
         'Environment :: Console',
